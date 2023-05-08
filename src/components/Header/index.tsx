fb2ae253cb37cf7cc1b0bad61ce0272d6652e75c
marcos-dev14 <mprogramador.10@gmail.com>
2023-05-08 08:14:40 -0300

    feat: projeto já bem avançado

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { 
+  BackButton,
+  BackIcon, 
+  Container, 
+  Logo 
+} from "./styles";
+
+import logoImage from "@assets/logo.png";
+
+interface HeaderProps {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
+  return (
+    <Container>
+      {showBackButton && 
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      }
+
+      <Logo source={logoImage} />
+    </Container>
+  );
+}
---
+++ src/screens/Groups/index.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
+
+import { Header } from '@components/Header';
+import { Highlight } from '@components/Highlight';
+import { GroupCard } from "@components/GroupCard";
+
+import { Container } from './styles';
+import { ListEmpty } from "@components/ListEmpty";
+
+export function Groups() {
+  const [groups, setGroups] = useState<string[]>(["Turma da Rocket"]);
+
+  return (
+    <Container>
+      <Header />
+
+      <Highlight 
+        title="Turmas"
+        subtitle="Jogue com sua turma"
+      />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+
+    </Container>
+  );
+}
